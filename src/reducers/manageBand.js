d755459275ab3e25d5b8c71b714cf567a063ddef
kellysciandra <kellysciandra@icloud.com>
2020-01-09 11:02:15 -0500

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,5 +1,18 @@
 export default function manageBand(state = {
   bands: [],
 }, action) {
-  return state
-};
+  switch (action.type) {
+    case 'ADD_BAND':
+
+    console.log('adding', action.band);
+
+    return {
+      ...state,
+      bands: [...state.bands, action.band]
+    };
+   
+    default: 
+    console.log(state)
+     return state;
+  }
+}
